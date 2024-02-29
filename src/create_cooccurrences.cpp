422f7b09385b422851c3f47509acd88b4ce7a756
Grady Arnold <gradyarnold11@gmail.com>
2024-02-29 13:17:57 -0500

    Some work done

---
+++ src/create_cooccurrences.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <unordered_map>
+#include <fstream>
+
+// Create vocabulary dictionary
+
+int main(int argc, char **argv) {
+    if (argc != 4) {
+        std::cerr << "USAGE: ./create_frequencies vocab.txt corpus.txt db_name\n";
+    }
+    
+    std::string in_string;
+    int freq;
+
+    std::ifstream in_file()
+    return EXIT_SUCCESS;
+}
---
+++ src/gen_frequencies.cpp
@@ -0,0 +1,43 @@
+#include <map>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <cstdint>
+
+int main(int argc, char **argv) {
+    if (argc != 4) {
+        std::cerr << "USAGE gen_frequencies corpus.txt output.txt min_count\n";
+        return EXIT_FAILURE;
+    }
+
+    std::map<std::string, uint32_t> vocab;
+
+    std::ifstream infile (argv[1]);
+    
+    if (!infile.is_open()) {
+        std::cerr << "Corpus not read properly. exiting...\n";
+    }
+
+    std::string word;
+    while (infile >> word) {
+        vocab[word]++;
+    }
+
+    std::ofstream outfile (argv[2]);
+
+    if (!outfile.is_open()) {
+        std::cerr << "Output not opened properly. exiting...\n";
+    }
+
+    int min_count = atoi(argv[3]);
+
+    for (auto &[word, freq] : vocab) {
+        if (freq >= min_count)
+            outfile << word << " " << freq << "\n";
+    }
+    
+    infile.close();
+    outfile.close();
+
+    return EXIT_SUCCESS;
+}
